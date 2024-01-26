ee8211a6834a33a67faaf2b662acfd82a6e25d72
psycho <codingturbo77@gmail.com>
2024-01-26 19:29:12 +0800

    更新

--- src/main/java/com/dx/controller/ChainWalletController.java
+++ src/main/java/com/dx/controller/ChainWalletController.java
@@ -91,7 +91,7 @@ public class ChainWalletController {
      * @return
      */
     @PostMapping("/hot-wallet/expenses")
-    public Result hotWalletExpenses(HotWalletExpensesVO vo){
+    public Result<HotWalletExpensesDTO> hotWalletExpenses(HotWalletExpensesVO vo){
         return walletService.hotWalletExpenses(vo);
     }
 
